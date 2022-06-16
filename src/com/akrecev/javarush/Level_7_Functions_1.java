b449c94e8da9f5d19a72ab92f21cc678ad1a4208
akrecev <akrecev@gmail.com>
2022-06-16 22:54:48 +0300

    new

--- src/com/akrecev/javarush/Level_7_Functions_1.java
+++ src/com/akrecev/javarush/Level_7_Functions_1.java
@@ -1,4 +1,13 @@
 package com.akrecev.javarush;
 
 public class Level_7_Functions_1 {
+    public static void main(String[] args) {
+        print();
+        print();
+        print();
+    }
+
+    public static void print() {
+        System.out.println("Тестовый метод печати print");
+    }
 }
